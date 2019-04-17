f78514bdfa7e90f176eeae2ba8836cb0fc6bc7a0
Adrien Caubel <adrien.caubel@iut-rodez.fr>
2019-04-17 11:37:59 +0200

    6.3 registration management

--- src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
+++ src/main/resources/db/migration/R__Functions_to_fix_activities_without_owner.sql
@@ -1,27 +1,34 @@
 CREATE OR REPLACE FUNCTION get_default_owner() RETURNS "user" AS $$
 	DECLARE
-		defaultOwner "user"%ROWTYPE;
-		defaultOwnerUsername VARCHAR(500) := 'Default Owner';
+		defaultOwner "user"%rowtype;
+		defaultOwnerUsername varchar(500) := 'Default Owner';
+		
 	BEGIN
-	    SELECT * INTO defaultOwner FROM "user" WHERE username = defaultOwnerUsername;
-    	IF not found THEN
-        	INSERT INTO "user" VALUES (nextval('id_generator'),defaultOwnerUsername);
-        	SELECT * INTO defaultOwner FROM "user" WHERE username = defaultOwnerUsername;
-    	END IF;
-    	RETURN defaultOwner;
-    END
-$$ LANGUAGE PLPGSQL;
-
+		SELECT * into defaultOwner
+		FROM "user" 
+		WHERE username = defaultOwnerUsername ;
+	
+		IF NOT FOUND THEN
+			INSERT INTO "user" (id, username) VALUES (nextval('id_generator'), 'Default Owner');
+			SELECT * into defaultOwner
+			FROM "user" 
+			WHERE username =defaultOwnerUsername ;
+		END IF;
+		RETURN defaultOwner;
+	END
+$$ LANGUAGE plpgSQL;
 
 CREATE OR REPLACE FUNCTION fix_activities_without_owner() RETURNS SETOF activity AS $$
-    DECLARE
-        defaultOwner "user"%ROWTYPE;
-    BEGIN
-        defaultOwner := get_default_owner();
-        RETURN QUERY
-        UPDATE activity
-        SET owner_id = defaultOwner.id
-        WHERE owner_id is null
-        RETURNING *;
-    END
-$$ LANGUAGE PLPGSQL;
+	DECLARE
+		defaultOwner "user"%rowtype;
+		nowDate date = now();		
+	BEGIN
+		defaultOwner := get_Default_Owner();
+		RETURN QUERY 
+			UPDATE activity 
+			SET owner_id = defaultOwner.id,
+			modification_date = nowDate
+				WHERE owner_id IS NULL
+			RETURNING *;		
+	END
+$$ LANGUAGE plpgSQL;
